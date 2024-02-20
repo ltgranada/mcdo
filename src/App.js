12f2a9762cd74106b413be4a481e37815fdc6e65
ltgranada <luisenrico.granada.cics@ust.edu.ph>
2024-02-20 14:25:41 +0800

    update

--- src/App.js
+++ src/App.js
@@ -4,19 +4,34 @@ import Header from "./Header.js";
 import Category from "./Category.js";
 import Quality from "./Quality.js";
 import Menu from "./Menu.js";
+import Menu2 from "./Menu2.js";
 import Footer from "./Footer.js";
 
 function App() {
-  return (
-    <div className="container">
-      <Navbar />
-      <Header />
-      <Category />
-      <Quality />
-      <Menu />
-      <Footer />
-    </div>
-  );
+  const isMenu = true;
+  if (isMenu) {
+    return (
+      <div className="container">
+        <Navbar />
+        <Header />
+        <Category />
+        <Quality />
+        <Menu2 />
+        <Footer />
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <Navbar />
+        <Header />
+        <Category />
+        <Quality />
+        <Menu />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
